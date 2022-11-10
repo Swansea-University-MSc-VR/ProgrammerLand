4956e1275ae8131eed7a688782f1fdfe84db32a7
Ted Thomas <e.l.thomas@Swansea.ac.uk>
2022-11-10 13:58:27 +0000

    more section 6

--- Assets/Scripts/Guest.cs
+++ Assets/Scripts/Guest.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Guest : MonoBehaviour
 {
@@ -9,15 +11,58 @@ public class Guest : MonoBehaviour
     public Ride targetRide; // ride the guest is currently trying to go on
     public bool isQueueing;
 
+    public Rubbish rubbishPrefab;
+
+    private NavMeshAgent _navMeshAgent;
+
+    private float _rubbishTime; // how long to wait untill guest drops rubbish
+    private float _rubbishTimer; // a timer that keeps track when the guest should drop gue
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(RandomNavmeshLocation(10f));
+
+        _rubbishTime = Random.Range(3f, 10f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_navMeshAgent.hasPath)
+        {
+            if(_navMeshAgent.remainingDistance < 0.5f)
+            {
+                _navMeshAgent.SetDestination(RandomNavmeshLocation(10f));
+            }
+        }
+
+        if(_rubbishTimer < _rubbishTime)
+        {
+            _rubbishTimer += Time.deltaTime;
+        }
+        else
+        {
+            Instantiate(rubbishPrefab, transform.position, Quaternion.identity);
+            _rubbishTimer = 0;
+            _rubbishTime = Random.Range(3f, 10f);
+        }
     }
+
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
+    }
+
+
 }
---
+++ Assets/Scripts/GuestFinder.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuestFinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Guest guest;
+
+        // find the guest
+        guest = GameObject.Find("Guest").GetComponent<Guest>();
+        Debug.Log(guest);
+
+        // find the guest using a tag
+        guest = GameObject.FindGameObjectWithTag("Guest").GetComponent<Guest>();
+        Debug.Log(guest);
+
+        // find the guest by type
+        guest = GameObject.FindObjectOfType<Guest>();
+        Debug.Log(guest);
+
+        // find the guest's face by using find from the transform class
+        GameObject face = guest.transform.Find("Face").gameObject;
+        Debug.Log(face);
+
+        // find the guest toy
+        GameObject nose = guest.transform.Find("Face/Nose").gameObject;
+        Debug.Log(nose);
+
+        // find the guest's mouth  by getChild
+        GameObject mouth = guest.transform.transform.Find("Face").GetChild(3).gameObject;
+        Debug.Log(mouth);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
